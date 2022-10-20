2ceb2ade399457ee2fcefeb097b5384aa5d0415b
SemenSavlook <semensavluk@gmail.com>
2022-10-20 16:16:24 +0600

    done 8-11

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,26 +4,27 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 
-import Row from '../row';
-import ItemDetails, { Record } from '../item-details';
-import ErrorIndicator from '../error-indicator';
-import SwapiService from '../../services/swapi-service';
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
+
+import {
+  PersonDetails,
+  PlanetDetails,
+  StarshipDetails,
+  PersonList,
+  PlanetList,
+  StarshipList
+} from '../sw-components';
 
 import './app.css';
-
 
 export default class App extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    showRandomPlanet: true,
-    hasError: false
+    showRandomPlanet: true
   };
-
-  componentDidCatch() {
-    this.state({ hasError: true });
-  }
 
   toggleRandomPlanet = () => {
     this.setState((state) => {
@@ -35,79 +36,60 @@ export default class App extends Component {
 
   render() {
 
-    if (this.state.hasError) {
-      return (
-        <div className="stardb-app container">
-        <ErrorIndicator />
-        </div>
-      )
-    }
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet /> :
+      null;
 
-    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-
-    const { getPerson, 
-            getStarship,
-            getPersonImage,
-            getStarshipImage } = this.swapiService;
+    const { getPerson,
+      getStarship,
+      getPersonImage,
+      getStarshipImage,
+      getAllPeople,
+      getAllPlanets } = this.swapiService;
 
     const personDetails = (
-      <ItemDetails 
+      <ItemDetails
         itemId={11}
         getData={getPerson}
         getImageUrl={getPersonImage} >
 
-        <Record field='gender' label='Gender: '/>
-        <Record field='eyeColor' label='Eye Color: '/>
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+
       </ItemDetails>
-    )
+    );
 
     const starshipDetails = (
       <ItemDetails
         itemId={5}
-        getData={getStarship} 
+        getData={getStarship}
         getImageUrl={getStarshipImage}>
 
-        <Record field='model' label='Model: ' />
-        <Record field='length' label='Length: ' />
-        <Record field='costInCredits' label='Cost: ' />
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
       </ItemDetails>
-    )
+    );
 
     return (
       <ErrorBoundry>
-        <div className="stardb-app container">
+        <div className="stardb-app">
           <Header />
 
-          <Row 
-            left={personDetails}
-            right={starshipDetails} />
+          <PersonDetails itemId={11} />
+
+          <PlanetDetails itemId={5} />
+
+          <StarshipDetails itemId={9} />
+
+          <PersonList />
+
+          <StarshipList />
+
+          <PlanetList />
+
         </div>
       </ErrorBoundry>
     );
   }
-};
-
-/// 8-6-04-09
-
-// {/* <div className="row mb-3 mt-4">
-//           <div className="col-md-6">
-//             <ItemList onItemSelected={ this.onPersonSelected } 
-//               getData={ this.swapiService.getAllPlanets } 
-//               renderItem={(item) => (<span>{item.name}</span>) }
-//               />
-//           </div>
-//           <div className="col-md-6">
-//             <PersonDetails personId={this.state.selectedPerson} />
-//           </div>
-//         </div> */}
-
-// {/* <div className="row mb-3 mt-4">
-//           <div className="col-md-6">
-//             <ItemList onItemSelected={ this.onPersonSelected } 
-//               getData={ this.swapiService.getAllStarships } 
-//               renderItem={ (item) => item.name } />
-//           </div>
-//           <div className="col-md-6">
-//             <PersonDetails personId={this.state.selectedPerson} />
-//           </div>
-//         </div> */}
+}
